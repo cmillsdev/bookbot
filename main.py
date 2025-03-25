927ac9d87c50a2f0ff3fc1cda5ba7c87ca635c68
Cody <vzualbandt@gmail.com>
2025-03-25 02:30:20 -0700

    A finished bookbot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_word_count
+import sys
+from stats import get_word_count, get_char_count, sort_char_dict
 
 def get_book_text(fp):
     with open(fp, "r") as f:
@@ -7,4 +8,21 @@ def get_book_text(fp):
     return file_contents
 
 def main():
-    get_word_count(get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
+    num_words = get_word_count(book_text)
+    num_char = get_char_count(book_text)
+    sorted_chars = sort_char_dict(get_char_count(book_text))
+
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {sys.argv[1]}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------
+    """)
+    for s in sorted_chars:
+        print(f"{s["char"]}: {s["count"]}")
+main()
--- stats.py
+++ stats.py
@@ -1,3 +1,26 @@
 def get_word_count(text):
-    num_words = len(text.split())
-    print(f"{num_words} words found in the document")
+    return len(text.split())
+
+def get_char_count(text):
+    char_dict = {}
+    for t in text:
+        if t.lower() not in char_dict:
+            char_dict[t.lower()] = 0
+        char_dict[t.lower()] += 1
+
+    return char_dict
+
+def sort_char_dict(char_dict):
+    char_list = []
+
+    def sort_on(dict):
+        return dict["count"]
+
+    for c in char_dict:
+        if c.isalpha():
+            char_list.append({"char":c, "count":char_dict[c]})
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
+    
+
